7d933029dd60b4c328dd778f0d53538f472eeb72
Francisco Júnior <franciscojdsjr@gmail.com>
2021-04-24 11:15:03 -0300

    aula1

--- src/br/com/alura/TestandoListas.java
+++ src/br/com/alura/TestandoListas.java
@@ -1,5 +1,8 @@
 package br.com.alura;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class TestandoListas {
 
     public static void main(String[] args) {
@@ -7,6 +10,47 @@ public class TestandoListas {
         String aula1 = "Conhecendo mais de listas";
         String aula2 = "Modelando a classe aula";
         String aula3 = "Trabalhando com Cursos e Sets";
+
+        ArrayList<String> aulas = new ArrayList<>();
+        aulas.add(aula1);
+        aulas.add(aula2);
+        aulas.add(aula3);
+
+        System.out.println(aulas);
+
+        aulas.remove(0);
+
+        System.out.println("____________________");
+        for (String aula : aulas) {
+
+            System.out.println("Aula: " + aula);
+
+        }
+
+        System.out.println("____________________");
+        String primeiraAula = aulas.get(0);
+        System.out.println("A primeira aula é " + primeiraAula);
+
+        System.out.println("____________________");
+        for (int i = 0; i < aulas.size(); i++) {
+
+            System.out.println("Aula: " + aulas.get(i));
+        }
+
+        System.out.println("____________________");
+        aulas.forEach(aula -> {
+
+            System.out.println("percorrendo:");
+            System.out.println("Aula " + aula);
+        });
+
+        System.out.println("____________________");
+
+        aulas.add("Aumentando nosso conhecimento");
+
+        Collections.sort(aulas);
+        System.out.println(aulas);
+
     }
 
 }
