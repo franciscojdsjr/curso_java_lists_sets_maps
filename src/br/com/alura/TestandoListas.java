f06648217fd4c9d0a5de9a93173f3a1dfe6eff69
Francisco Júnior <franciscojdsjr@gmail.com>
2021-04-24 10:42:06 -0300

    aula1

---
+++ src/br/com/alura/TestandoListas.java
@@ -0,0 +1,12 @@
+package br.com.alura;
+
+public class TestandoListas {
+
+    public static void main(String[] args) {
+
+        String aula1 = "Conhecendo mais de listas";
+        String aula2 = "Modelando a classe aula";
+        String aula3 = "Trabalhando com Cursos e Sets";
+    }
+
+}
