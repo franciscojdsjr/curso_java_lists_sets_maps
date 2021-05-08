4744e141d6cbedbb51ee8052c103627e764a713f
Francisco Júnior <franciscojdsjr@gmail.com>
2021-05-08 18:33:01 -0300

    aula5

---
+++ src/br/com/alura/TestaAlunos.java
@@ -0,0 +1,19 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class TestaAlunos {
+
+    public static void main(String[] args) {
+
+        Set<String> alunos = new HashSet<>();
+
+        alunos.add("Francisco José");
+        alunos.add("José Leandro");
+        alunos.add("Edson");
+        alunos.add("Paulo Silveira");
+
+        System.out.println(alunos);
+
+    }
+
+}
